dfaaac73784964ca6baf5bd31e06b62b776dbf37
Samyak1401 <samyak.sanghavi1401@gmail.com>
2024-09-27 12:30:49 +0530

    commited

---
+++ database/migrations/2024_09_27_065719_create_customer_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id("Customer_id");
+            $table->string("Customer_Fname", 60);
+            $table->string("Customer_Lname", 60);
+            $table->string("Customer_Email", 150)->unique();
+            $table->string("Customer_Password", 100);
+            $table->enum('role', ['customer', 'admin'])->default('customer');
+            $table->timestamp("email_verfied_at")->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customer');
+    }
+};
