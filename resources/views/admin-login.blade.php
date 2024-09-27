115488227a17a8eb4d50af5cc245769b28618ee5
Samyak1401 <samyak.sanghavi1401@gmail.com>
2024-09-27 13:12:06 +0530

    committed

--- resources/views/admin-login.blade.php
+++ resources/views/admin-login.blade.php
@@ -7,6 +7,13 @@
     <title>Document</title>
 </head>
 <body>
-    Admin login
+    <form action="" method="POST">
+        @csrf
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email"><br><br>
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password">
+        <button type="submit">Login</button>
+    </form>
 </body>
 </html>
