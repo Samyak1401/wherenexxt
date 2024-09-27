caee3df6b40415581c5893c7338a31acb59a1f0c
Samyak1401 <samyak.sanghavi1401@gmail.com>
2024-09-27 12:26:21 +0530

    "Commented out root route and added closing bracket at end of file"

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
 
@@ -14,4 +14,4 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-});
+});*/
